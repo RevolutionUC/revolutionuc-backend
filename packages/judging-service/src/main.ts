444abab09b489c5a364708e58d8c973e0e85c4a2
Dev Agrawal <devagrawal09@gmail.com>
2021-10-06 01:25:30 -0400

    listening to $PORT

--- packages/judging-service/src/main.ts
+++ packages/judging-service/src/main.ts
@@ -3,10 +3,11 @@ config();
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { environment } from './environment';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(environment.PORT);
 }
 bootstrap();
