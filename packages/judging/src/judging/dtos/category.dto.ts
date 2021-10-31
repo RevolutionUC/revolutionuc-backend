01299fe844e6a4e2bf715e9f952d9b0d9bca40db
Dev Agrawal <agrawadv@mail.uc.edu>
2021-10-31 13:49:29 -0400

    removed judgind

--- packages/judging/src/judging/dtos/category.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CategoryDto {
-  name: string;
-  groupCount: number;
-  groupsPerSubmission: number;
-  mandatory?: boolean;
-}
--- packages/judging/src/util/index.ts
+++
@@ -1,3 +0,0 @@
-export { devpostParser } from './devpost-export-parser';
-export { groupNameGenerator } from './group-name-generator';
-export { shuffleArray } from './shuffle-array';
